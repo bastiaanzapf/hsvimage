09a1cdd0e4705aed80be679588d69c4b9e6642a5
Bastiaan Zapf <bzapf@online.de>
2014-12-21 20:23:31 +0100

    Split into functions
    (Octave needs references)

--- hsvimage.cc
+++ hsvimage.cc
@@ -2,6 +2,69 @@
 
 #include <octave/oct.h>
 
+int check_arguments(octave_value_list args) {
+  if (args(0).is_real_matrix()) {
+    if (args.length()!=3) {
+      error("hsvimage(A,B,C): first parameter is real, so exactly three parameters are required");
+      return 0;
+    }
+    if (!args(1).is_real_matrix() ||
+	!args(2).is_real_matrix()) {
+      error("hsvimage(A,B,C): all arguments must be real matrices");
+      return 0;
+    }
+    return 1;
+  }
+
+  if (args(0).is_complex_matrix()) {
+    if (args.length()!=1) {
+      error("hsvimage(A): first parameter is complex, so exactly one parameter is required");
+      return 0;
+    }
+    return 2;
+  }
+
+  if (args(0).is_real_matrix()) {
+    dim_vector dims=args(0).dims();
+    if (dims(3)!=3) {
+      error("hsvimage(A): first parameter is real and there are no other parameters, so A should have a third dimension of 3");
+      return 0;
+    }
+    return 3;
+  }
+
+  error("hsvimage: illegal arguments");      
+  
+  return 0;
+}
+
+void setRGB(NDArray &x, int i, int j, double a, double b, double c) {
+  x(i,j,0)=a;
+  x(i,j,1)=b;
+  x(i,j,2)=c;
+}
+
+void setHSV(NDArray &x,int i, int j,double H, double S, double V) {
+  int h_i=(int)(6*H);
+  float f=(6*H)-h_i;
+  float p=0;
+  float q=V*(1-f);
+  float t=V*f;
+  if ( h_i==0 || h_i==6 ){
+    setRGB(x,i,j,V,t,p);
+  } else if (h_i==1) {
+    setRGB(x,i,j,q,V,p);
+  } else if (h_i==2) {
+    setRGB(x,i,j,p,V,t);
+  } else if (h_i==3) {
+    setRGB(x,i,j,p,q,V);
+  } else if (h_i==4) {
+    setRGB(x,i,j,t,p,V);
+  } else if (h_i==5) {
+    setRGB(x,i,j,V,p,q);
+  }
+}
+
 DEFUN_DLD (hsvimage, args, ,
 "Display an image from hsv data.\n"
 "\n"
@@ -20,93 +83,30 @@ DEFUN_DLD (hsvimage, args, ,
   int arguments=args.length();
   octave_value_list retval;
 
-  int mode=0;
-
-  if (args(0).is_real_matrix()) {
-    if (args.length()!=3) {
-      error("hsvimage(A,B,C): first parameter is real, so exactly three parameters are required");
-      return retval;
-    }
-    if (!args(1).is_real_matrix() ||
-	!args(2).is_real_matrix()) {
-      error("hsvimage(A,B,C): all arguments must be real matrices");
-      return retval;
-    }
-    mode=1;
-  }
-
-  if (mode==0 && args(0).is_complex_matrix()) {
-    if (args.length()!=1) {
-      error("hsvimage(A): first parameter is complex, so exactly one parameter is required");
-      return retval;
-    }
-    mode=2;
-  }
-
-  if (mode==0 && args(0).is_real_matrix()) {
-    dim_vector dims=args(0).dims();
-    if (dims(3)!=3) {
-      error("hsvimage(A): first parameter is real and there are no other parameters, so A should have a third dimension of 3");
-      return retval;
-    }
-    mode=3;
-  }
-
-  if (mode == 0) {
-    error("hsvimage: illegal arguments");      
-    return retval;
-
-  }
+  int mode=check_arguments(args);
 
   int width=args(0).columns();
   int height=args(0).rows();
 
-  dim_vector dv(width,height,3);
+  dim_vector dv(height,width,3);
 
   NDArray image(dv);
 
   ComplexMatrix arg0=args(0).complex_matrix_value();
 
-  for (int i=0;i<width;i++) {
-    for (int j=0;j<height;j++) {
+  for (int i=0;i<height;i++) {
+    for (int j=0;j<width;j++) {
       //      if (mode==1)
       Complex arg0_ = ((octave_value)arg0(i,j)).complex_value();
-      float H=arg(arg0(i,j));
-      int h_i=(int)(3+(3*H)*(M_1_PI));
-      float f=(3+(3*H)*(M_1_PI))-h_i;
-      octave_stdout << h_i << "\n";
+
+      float H=0.5+(arg(arg0(i,j)))*(M_1_PI);
+      float S=1;
       float V=abs(arg0(i,j));
-      float p=0;
-      float q=V*(1-f);
-      float t=V*f;
-      if ( h_i==0 || h_i==6 ){
-	image(i,j,0)=V;
-	image(i,j,1)=t;
-	image(i,j,2)=p;
-      } else if (h_i==1) {
-	image(i,j,0)=q;
-	image(i,j,1)=V;
-	image(i,j,2)=p;	
-      } else if (h_i==2) {
-	image(i,j,0)=p;
-	image(i,j,1)=V;
-	image(i,j,2)=t;	
-      } else if (h_i==3) {
-	image(i,j,0)=p;
-	image(i,j,1)=q;
-	image(i,j,2)=V;	
-      } else if (h_i==4) {
-	image(i,j,0)=t;
-	image(i,j,1)=p;
-	image(i,j,2)=V;	
-      } else if (h_i==5) {
-	image(i,j,0)=V;
-	image(i,j,1)=p;
-	image(i,j,2)=q;	
-      }
+
+      setHSV(image,i,j,H,S,V);
     }
   }
-
+  
   retval(0)=image;
 
   return retval;
